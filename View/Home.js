2871ae70b46eeb2eeae64f665d882d6738a4f80b
leehojun99 <hj81179@gmail.com>
2021-03-18 18:35:40 +0900

    5일차

--- View/Home.js
+++ View/Home.js
@@ -1,30 +1,27 @@
-import React from 'react';
-import { StyleSheet, View, ScrollView } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, View, ScrollView, Alert, Text } from 'react-native';
+import axios from 'axios';
 
 import TopBar from '../Components/TopBar';
 import ThreadItem from '../Components/ThreadItem';
 
-const func23 = () => {
-    alert('123123123123', 'asdf');
-};
-
-
-
 
 export default function Home({navigation}) {
-    return(
-        <View>
-            <TopBar/>
-            <ScrollView>
-                <ThreadItem onPress={() => {navigation.navigate('Order')}} name={"이름123"} major={"컴정"} isSale={false}/>
-                <ThreadItem onPress={() => {alert('123123')}} name={"이름456"} major={"경호"} isSale={true}/>
-                <ThreadItem name={"이름789"} major={"멀티"} isSale={false}/>
-                <ThreadItem name={"이름012"} major={"기계"} isSale={true}/>
-                <ThreadItem name={"이름345"} major={"전자"} isSale={false}/>
-                <ThreadItem name={"이름678"} major={"회계"} isSale={true}/>
-                <ThreadItem name={"이름901"} major={"기계"} isSale={false}/>
-            </ScrollView>
-        </View> 
-    );
+  
+  const threadItems = [{"name":"김소현","major":"호텔 조리","bookName":"조리학","description":"조리학 책 구매 희망","isSell":true,"imageUrl":"https://~~!!"},{"name":"이호준","major":"컴정","bookName":"네트워크","description":"판매 합니다","isSell":false,"imageUrl":"https://~~!!"},{"name":"김명조","major":"멀티미디어","bookName":"행복한 가정만들기","description":"교양 책 구매","isSell":true,"imageUrl":"https://~~!!"},{"name":"아바타","major":"경호","bookName":"채픔","description":"채플책 삽니다.","isSell":true,"imageUrl":"https://~~!!"},{"name":"인지훈","major":"기계","bookName":"기계 공학","description":"기계 공학 책 판매","isSell":false,"imageUrl":"https://~~!!"},{"name":"신인성","major":"예체능","bookName":"스포츠 농구","description":"스포츠 농구 책 판매","isSell":false,"imageUrl":"https://~~!!"}];
+
+  
+
+  return (
+      <View style={{flex: 1}}>
+          <TopBar/>
+          <ScrollView>
+            {threadItems.map((item, index) => (
+              <ThreadItem onPress={() => {navigation.navigate('Order')}} name={item.name} major={item.major} isSale={!item.isSell} bookName={item.bookName} description={item.description}/>
+            ))}
+          </ScrollView>
+      </View> 
+  );
+    
 }
 
