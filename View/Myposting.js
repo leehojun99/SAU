8886bcc2758c0f4610f50160f5fdafaa02a77ff9
leehojun99 <hj81179@gmail.com>
2021-09-03 15:06:42 +0900

    마이페이지 내가쓴글보기 내가 쓴글들만 보이게 수정

--- View/Myposting.js
+++ View/Myposting.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 import {
@@ -8,15 +8,65 @@ import {
   ScrollView,
   SafeAreaView,
   TouchableOpacity,
+  RefreshControl,
 } from "react-native";
-import { useUserContext } from "../UserContext";
 
-export default function Order({ navigation, route }) {
-  let server = "https://api.saubook.store/"; //도메인 주소  바뀔 일 없음 .
+import ThreadItem from "../Components/ThreadItem";
+import { useUserContext } from "../UserContext.js";
+
+export default function Myposting({ navigation, route }) {
+  let server = "https://api.saubook.store"; //도메인 주소  바뀔 일 없음 .
+  const { user, setUser } = useUserContext();
+
+  useEffect(() => {
+    if (threadItems.length == 0) reloadTimeline();
+    console.log(filterData);
+  }); //처음 들가면 화면 새로고침
+
+  const settingFilter = (filterData) => {
+    setRefreshing(true); // 새로고침 시작
+    setFilter(filterData);
+
+    if (filterData.token != "")
+      axios
+        .get(server + "/post/search?type=token&token=" + filterData.token) // 서버에서 타임라인 가져오기
+        .then(function (response) {
+          console.log(response.data); // 내용 뿌려주기
+          setThreadItems(response.data);
+          setRefreshing(false); // 새로고침 끝남
+        });
+    else
+      axios
+        .get(server + "/post/live?page=1") // 서버에서 타임라인 가져오기
+        .then(function (response) {
+          console.log(response.data); // 내용 뿌려주기
+          setThreadItems(response.data);
+          setRefreshing(false); // 새로고칢 끝남
+        });
+  };
+
+  const [threadItems, setThreadItems] = useState([]);
+  const [filterData, setFilter] = useState({ token: "" });
+
+  const [refreshing, setRefreshing] = React.useState(false);
+
+  const reloadTimeline = () => {
+    setRefreshing(true); // 새로고침 시작
+    console.log(server + "/user/myposts?userToken=" + user);
+    axios
+      .get(server + "/user/myposts?userToken=" + user) // 서버에서 타임라인 가져오기
+      .then(function (response) {
+        console.log(response.data); // 내용 뿌려주기
+        setThreadItems(response.data);
+        setRefreshing(false); // 새로고칢 끝남
+      });
+  }; // 새로고침 실시간
 
   return (
-    <View style={styles.ccc}>
-      <Text>Myposting</Text>
+    <View style={{ flex: 1 }}>
+      <View style={styles.TopContainer}>
+        <Text style={styles.Topfont}>Myposting</Text>
+      </View>
       <View style={styles.Logout}>
         <TouchableOpacity
           activeOpacity={0.8}
@@ -28,11 +78,50 @@ export default function Order({ navigation, route }) {
           <Text style={styles.logoutText}> Log Out </Text>
         </TouchableOpacity>
       </View>
+      <View reload={reloadTimeline} filter={settingFilter} />
+      <ScrollView
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={reloadTimeline} />
+        }
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        {threadItems.map((item, index) => (
+          <ThreadItem
+            onPress={() => {
+              navigation.navigate("Order", { item: item });
+            }}
+            name={item.name}
+            major={item.major}
+            isSale={!item.isSell}
+            title={item.title}
+            description={item.description}
+            price={item.price}
+            imageUri={item.imageUri}
+          />
+        ))}
+      </ScrollView>
     </View>
   );
 }
 const styles = StyleSheet.create({
-  ccc: {
+  Container: {
     flex: 1,
+  },
+  TopContainer: {
+    padding: 10,
+    width: 180,
+    height: 80,
+    backgroundColor: "#FFF",
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 10,
+    marginLeft: 30,
+    borderRadius: 50,
+    borderWidth: 5,
+    borderColor: "red",
+  },
+  Topfont: {
+    fontSize: 25,
+    fontWeight: "bold",
   },
 });
